ad42937368dc1627d0254d98809a24050af38c74
Sameer Hussain <105971029+sam12-4@users.noreply.github.com>
2025-01-09 22:45:58 +0500

    connection change

--- data/database.js
+++ data/database.js
@@ -5,8 +5,7 @@ export const connectDB = async () => {
         await mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        });
-        console.log('MongoDB connected');
+        }).then((e) => console.log("Connected to database",e.connection.host));
     } catch (error) {
         console.error(error.message);
         process.exit(1);
